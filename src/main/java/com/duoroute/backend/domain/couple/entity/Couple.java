de597d69b26e5445a55f5a0dd4de9b68afa69155
tylee <marine4391@naver.com>
2026-04-23 15:50:07 +0900

    fix: env 파일 설정 및 couple entity 수정

--- src/main/java/com/duoroute/backend/domain/couple/entity/Couple.java
+++ src/main/java/com/duoroute/backend/domain/couple/entity/Couple.java
@@ -1,31 +1,28 @@
 package com.duoroute.backend.domain.couple.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "couples")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class Couple {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Builder.Default
     @Column(nullable = false, length = 20)
-    private String status; // ACTIVE(연결됨), BROKEN(헤어짐)
+    private String status = "ACTIVE"; // ACTIVE(연결됨), BROKEN(헤어짐)
 
+    @Builder.Default
     @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
 
-    @Builder
-    public Couple() {
-        this.status = "ACTIVE";
-        this.createdAt = LocalDateTime.now();
-    }
 }
